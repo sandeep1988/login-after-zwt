8d1aef1110ad7c8876d1db977c61b9d86ef02c51
sandeep <schourey1988@gmail.com>
2015-07-04 21:55:43 +0530

    login ajax password forgot

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -24,8 +24,8 @@ end
 
 def destroy
     redirect_to root_path
-  end
-  
+end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     # Never trust parameters from the scary internet, only allow the white list through.
