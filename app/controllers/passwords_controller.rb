dc99d708776bf589da823e143bd89cee17c239ed
sandeep <schourey1988@gmail.com>
2015-07-06 13:00:50 +0530

    password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -21,7 +21,17 @@ class PasswordsController < Devise::PasswordsController
   end
 
   def update
-    redirect_to root_path
+    self.resource = resource_class.reset_password_by_token(resource_params)
+
+    if resource.errors.empty?
+      resource.unlock_access! if unlockable?(resource)
+      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+      set_flash_message(:notice, flash_message) if is_navigational_format?
+
+      respond_with resource, :location => after_resetting_password_path_for(resource)
+    else
+      respond_with resource
+    end
   end
 
   private
@@ -30,8 +40,24 @@ class PasswordsController < Devise::PasswordsController
   end
 
  protected
-  def after_sending_reset_password_instructions_path_for(resource_name)
-    new_user_session_path
-  end
+    # The path used after sending reset password instructions
+   def after_resetting_password_path_for(resource)
+     root_path(resource)
+   end
 
+    # Check if a reset_password_token is provided in the request
+    def assert_reset_token_passed
+        if params[:reset_password_token].blank?
+            set_flash_message(:error, :no_token)
+            redirect_to new_session_path(resource_name)
+        end
+    end
+
+    # Check if proper Lockable module methods are present & unlock strategy
+    # allows to unlock resource on password reset
+    def unlockable?(resource)
+        resource.respond_to?(:unlock_access!) &&
+        resource.respond_to?(:unlock_strategy_enabled?) &&
+        resource.unlock_strategy_enabled?(:email)
+    end
 end
