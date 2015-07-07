27b723d94bc02bb07e72b79427620ce42f6e0d53
sandeep <schourey1988@gmail.com>
2015-07-07 16:15:52 +0530

    remove ajax for login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,15 @@ def new
 def create
   # check user
 	if user_signed_in?
-		#redirect_to authenticated_root_path
-    result = [:status => "Success", :success_url => authenticated_root_path ]
-    render json: result
+		redirect_to authenticated_root_path
+    ### comment this for remove ajax
+    # result = [:status => "Success", :success_url => authenticated_root_path ]
+    # render json: result
 	else
-    #redirect_to root_path
-    result = [:status => "Error", :message => "Invalid Credential"]
-    render json: result
+    redirect_to root_path
+    ### comment this for remove ajax
+    # result = [:status => "Error", :message => "Invalid Credential"]
+    # render json: result
   end
 end
 
