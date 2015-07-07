42f9d59d4bc953a973aa52844195528d6a2a7a65
sandeep <schourey1988@gmail.com>
2015-07-07 14:43:45 +0530

    profile page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,18 +9,17 @@ class RegistrationsController  < Devise::RegistrationsController
     @user = User.new(user_params)
     if @user.save
       # Deliver the signup email
-      #UserMailer.welcome_email(@user).deliver
+      UserMailer.welcome_email(@user).deliver
       redirect_to dashboard_path
     else
       render :action => 'new'
     end
   end
 
-  def update
-    super
+def update
+  super
    # self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
    #  prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-
    #  resource_updated = update_resource(resource, account_update_params)
    #  yield resource if block_given?
    #  if resource_updated
@@ -34,7 +33,7 @@ class RegistrationsController  < Devise::RegistrationsController
    #  else
    #    clean_up_passwords resource
    #    respond_with resource
-   #  end
+    #end
   end
 
   def edit
@@ -50,12 +49,12 @@ class RegistrationsController  < Devise::RegistrationsController
       params.require(:user).permit(:email, :password, :password_confirmation, :e_status, :v_firstname, :v_lastname, :v_im_skype, :v_im_password, :v_gmail, :v_linkedin_url, :v_phone, :e_type)
     end
 
+
   protected
      def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:v_firstname, :v_lastname, :v_im_skype, :v_im_password, :v_phone, :v_gmail, :v_linkedin_url)
       end
-
      devise_parameter_sanitizer.for(:account_update) do |u|
         u.permit(:v_firstname,
           :v_lastname, :v_im_skype, :v_im_password, :v_phone, :email, :password, :v_gmail, :v_linkedin_url)
@@ -66,4 +65,16 @@ class RegistrationsController  < Devise::RegistrationsController
     def update_resource(resource, params)
       resource.update_without_password(params)
     end
+
+ private
+
+  def sign_up_params
+    params.require(:user).permit(:v_firstname, :v_lastname, :email, :password, :v_im_skype, :v_im_password, :v_phone, :v_gmail, :v_linkedin_url)
+  end
+
+private
+  def account_update_params
+    params.require(:user).permit(:v_firstname, :v_lastname, :email, :password, :v_im_skype, :v_im_password, :v_phone, :v_gmail, :v_linkedin_url, :v_phone, :e_type)
+  end
+
   end
