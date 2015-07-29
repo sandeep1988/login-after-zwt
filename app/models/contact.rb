7eecbd027f375b7431dff53f9d0c7de3245ba304
sandeep <schourey1988@gmail.com>
2015-07-29 18:00:14 +0530

    work on pagination

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,4 @@
 class Contact < ActiveRecord::Base
 	belongs_to :user
 	enum contact_type: [ :end_client, :outsourcing_firm]  #default 1 = End Client'
-	
-	def to_bool
-    return true if self == true || self =~ (/^(true|t|yes|y|1)$/i)
-    return false if self == false || self.blank? || self =~ (/^(false|f|no|n|0)$/i)
-    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
-  end
 end
